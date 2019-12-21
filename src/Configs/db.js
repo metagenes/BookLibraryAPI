df9a03eb7e03f6f2bd6b8bfceec6c7c452ed8e01
metagenes <48299631+metagenes@users.noreply.github.com>
2019-12-21 22:34:16 +0700

    deploy

--- src/Configs/db.js
+++ src/Configs/db.js
@@ -1,26 +1,11 @@
-// const mysql = require ('mysql');
-
-// const db = mysql.createConnection({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_DATABASE,
-// });
-
-// db.connect(err =>{
-//     if (err) {throw err} else {console.log('database is connected')}
-// });
-
-// module.exports = db;
-
 require("dotenv").config()
 
 const mysql = require('mysql');
 
-const db = mysql.createConnection({
-	host: 'remotemysql.com',
-	user: 'BdVnpDxf3K',
-	password: '3PstTjbiHS',
-    database: 'BdVnpDxf3K',
+const db = mysql.createPool({
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME
 });
 module.exports = db;
